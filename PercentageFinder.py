5aea092f58c79aa2f45cf4033148ee317c013279
Princegaming9026 <93808464+Princegaming9026@users.noreply.github.com>
2023-07-16 05:27:08 +0530

    Add files via upload

---
+++ PercentageFinder.py
@@ -0,0 +1,26 @@
+
+print("School Percentage Finder")
+
+print("make by prince yadav")
+
+
+
+print("       ")
+
+
+mathMarks = input("Enter Your Math Marks--->")
+physicsMarks = input("Enter Your physics Marks--->")
+englishMarks = input("Enter Your English Marks--->")
+chemistryMarks = input("Enter Your Chemistry Marks--->")
+hindiMarks = input("Enter Your Hindi Marks--->")
+sum = int(mathMarks) + int(physicsMarks) + int(englishMarks) + int(chemistryMarks) + int(hindiMarks)
+print("total: ",sum)
+
+("total:",sum*100)
+
+
+
+print("Your Percentage:",sum*100/500)
+
+
+print("it's creater was PRINCE ")
